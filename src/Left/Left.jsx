139710bb4d345d7e6ddfa3142d56443d36dbf507
Ekaterina <vam.pochta@yahoo.com>
2023-04-06 16:40:21 +0400

    adds adaptive

--- src/Left/Left.jsx
+++ src/Left/Left.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 
 import styles from './Left.module.scss';
 
-const url = '../assets/images/me.png'
+const age = new Date().getFullYear() - new Date(1990, 0, 28).getFullYear();
 
 const LeftContent = () => {
 	return (
 		<div className={styles.Left}>
-			<div className={styles.Left__ImageWrapper} />
+			<div className={styles.Left__TitleInfo}>
+				<div className={styles.Left__ImageWrapper}>
+					<div className={styles.Left__Image} />
+				</div>
+				<section className={styles.Left__TitleName}>
+					<h1 className={styles.Left__MainName}>Еремина Екатерина</h1>
+					<h2 className={styles.Left__JobTitle}>Фронтенд-разработчик</h2>
+				</section>
+			</div>
 
 			<section className={styles.Left__Contacts}>
 				<h1 className={styles.Left__Title}>Обо мне</h1>
+				<div className={styles.Left__Line}>
+				<span className={styles.Left__Label}>возраст</span>
+					<p className={styles.Left__Contact}>{age}</p>
+				</div>
 				<div className={styles.Left__Line}>
 					<span className={styles.Left__Label}>e-mail</span>
 					<p className={styles.Left__Contact}>vam.pochta@yahoo.com</p>
